035ff4ae9ed4fae804b87962963af6bad1778704
Luis <62965678+Tiquillo@users.noreply.github.com>
2020-07-22 10:53:13 -0600

    upgrades

--- asdfg.py
+++ asdfg.py
@@ -1,7 +1,11 @@
 import cv2
+import face_recognition
+import time
 guardado = False
 
-user = "lucho el del cartucho bb"
+user = "luis"
+
+known_image = face_recognition.load_image_file("./files/faceRecognitionFiles/"+user+".jpg")
 
 class Registro:
     def __init__(self):
@@ -10,14 +14,27 @@ class Registro:
     
     def Recognition(self):
         global guardado
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        face_cascade = cv2.CascadeClassifier('files/haarcascade_frontalface_default.xml')
 
         cap = cv2.VideoCapture(0)
 
         while not guardado:
+            start=time.time()
             ret, self.img = cap.read()
             gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            try:
+                known_ecoding = face_recognition.face_encodings(known_image)[0]
+                unknown_encoding = face_recognition.face_encodings(self.img)[0]
+            except:
+                pass
+
+            try:
+                results = face_recognition.compare_faces([known_ecoding], unknown_encoding)
+            except IndexError:
+                pass
+            print(results)
 
             for (x,y,w,h) in faces:
                 cv2.rectangle(self.img,(x,y),(x+w,y+h),(0,255,0),2)
@@ -25,6 +42,9 @@ class Registro:
                 roi_color = self.img[y:y+h, x:x+w]
 
             cv2.imshow('Reconociendo...', self.img)
+
+
+            print("loop time", time.time() - start)
             if cv2.waitKey(20) & 0xFF == ord("q"):
                 break
 
